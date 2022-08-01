e3cc2e0755fc05a6e2c2d46815364e23b7cb39c2
Mcavantfnonono <iovxkl85mm@163.com>
2022-08-01 21:21:24 +0800

    code

---
+++ code/day 1/helloword/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloword
+{
+    class Program
+    {
+        static void Main(string[] args){
+            //在这里写代码哦！！！！！
+              //（￣︶￣）↗　（￣︶￣）↗　（￣︶￣）↗　（￣︶￣）↗　
+            Console.WriteLine("打印HelloWorld");
+            Console.ReadKey();
+
+            Console.WriteLine("Console.WriteLine(\"HelloWorld\")");
+            Console.ReadKey();
+
+            ///.....转义符 \" 表示一个英文半角"
+            Console.WriteLine("HelloWorld");
+            Console.ReadKey();
+            Console.WriteLine("=======================================================");
+
+            Console.WriteLine( "Console 控制台\n WriteLine 写一行\n \t");
+            /* 
+            Console 控制台 
+            WriteLine 写一行("HelloWorld");
+            */
+            Console.WriteLine("=======================================================");
+
+            Console.ReadKey();
+            //.ReadKey();暂停当前程序，等待按下任意键继续按下的键显示在控制台中
+                 Console.WriteLine("11111111111111111111111111111111111111111111111");
+                 Console.ReadKey();
+                 Console.WriteLine("111111111111111111111111111111111111111111111111111111111");
+                 Console.ReadKey();//return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
